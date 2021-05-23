6814ce221b60c34ca35a6ea3abfab2507c7ea30c
Tejpratap <tejpratapsingh545@outlook.com>
2021-05-23 22:09:44 +0530

    authentocation and  create superadmin

--- src/server/backend/users/models.py
+++ src/server/backend/users/models.py
@@ -4,7 +4,14 @@ from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.db import models, transaction
 from django.utils import timezone
 
+from .backend import check_password_strength
+from .manager import UserManager
+
 # Create your models here.
+
+
+class PasswordTooWeakError(Exception):
+    pass
 
 
 class Company(models.Model):
@@ -14,7 +21,7 @@ class Company(models.Model):
         upload_to="company/login_logo", blank=True, null=True
     )
     domain = models.URLField(unique=True, blank=False, null=False)
-    setting = models.JSONField()
+    setting = models.JSONField(blank=True, null=True)
     is_active = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -25,14 +32,20 @@ class Company(models.Model):
 
 class Permission(models.Model):
     name = models.CharField(max_length=100, unique=True)
+    company = models.ForeignKey(
+        Company, on_delete=models.CASCADE, blank=True, null=True
+    )
 
     def __str__(self):
         return f"permission : {self.name}"
 
 
 class Role(models.Model):
-    name = models.CharField()
+    name = models.CharField(max_length=20)
     permissions = models.ManyToManyField(Permission)
+    company = models.ForeignKey(
+        Company, on_delete=models.CASCADE, blank=True, null=True
+    )
 
     def __str__(self):
         return f"role : {self.name}"
@@ -44,7 +57,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     username = models.CharField(max_length=50, unique=True, null=False)
     last_name = models.CharField(max_length=50, blank=True, null=True)
     email = models.EmailField(blank=True, null=False, unique=True)
-    contact_number = models.CharField(blank=True, null=True, unique=True)
+    contact_number = models.CharField(max_length=20, blank=True, null=True, unique=True)
 
     is_active = models.BooleanField(default=True)
     is_admin = models.BooleanField(default=False)
@@ -52,9 +65,12 @@ class User(AbstractBaseUser, PermissionsMixin):
     is_email_verified = models.BooleanField(default=False, null=True)
     date_joined = models.DateTimeField(auto_now_add=True)
     dummy_password = models.CharField(max_length=10, blank=True, null=True)
-    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+    company = models.ForeignKey(
+        Company, on_delete=models.CASCADE, blank=True, null=True
+    )
 
     USERNAME_FIELD = "username"
+    objects = UserManager()
 
     @property
     def is_staff(self):
@@ -72,6 +88,9 @@ class User(AbstractBaseUser, PermissionsMixin):
             self.set_unusable_password()
             return
 
+        if not check_password_strength(password):
+            raise PasswordTooWeakError
+
         super().set_password(password)
 
 
@@ -79,7 +98,13 @@ class Department(models.Model):
     name = models.CharField(max_length=100)
     company = models.ForeignKey(Company, on_delete=models.CASCADE)
     logo = models.ImageField(upload_to="department/logo", blank=True, null=True)
-    manager = models.ForeignKey("Profile", blank=True, null=True)
+    manager = models.ForeignKey(
+        "Profile",
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+        related_name="departments",
+    )
 
     def __str__(self):
         return f"department : {self.name}"
@@ -89,8 +114,8 @@ class Profile(models.Model):
 
     EMPLOYMENT_TYPE_CHOICE = (
         ("Contractor", "CONTRACTOR"),
-        ("Full-time", "FULL-TIME"),
-        ("Part-time", "PART-TIME"),
+        ("Full-Time", "FULL-TIME"),
+        ("Part-Time", "PART-TIME"),
         ("Internship", "INTERN"),
     )
 
@@ -109,13 +134,17 @@ class Profile(models.Model):
 
     # persional information
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    avatar = models.ImageField(upload_to="avatar")
+    avatar = models.ImageField(upload_to="avatar", blank=True, null=True)
 
     gender = models.CharField(
         max_length=6, choices=GENDER_CHOICE, blank=True, null=True
     )
     martial_status = models.CharField(
-        max_length=50, choices=MARITAL_STATUS_CHOICE, blank=True, null=True
+        max_length=50,
+        choices=MARITAL_STATUS_CHOICE,
+        default="Single",
+        blank=True,
+        null=True,
     )
 
     # employee information
@@ -126,7 +155,11 @@ class Profile(models.Model):
         Department, on_delete=models.SET_NULL, blank=True, null=True
     )
     employment_type = models.CharField(
-        max_length=50, choices=EMPLOYMENT_TYPE_CHOICE, blank=True, null=True
+        max_length=50,
+        choices=EMPLOYMENT_TYPE_CHOICE,
+        default="Full-Time",
+        blank=True,
+        null=True,
     )
     job_title = models.CharField(max_length=100, blank=True, null=True)
 
@@ -135,14 +168,14 @@ class Profile(models.Model):
         on_delete=models.SET_NULL,
         blank=True,
         null=True,
-        related_name="first_reporting_manager",
+        related_name="first_reporting_employees"
     )
     second_reporting_manager = models.ForeignKey(
         "Profile",
         on_delete=models.SET_NULL,
         blank=True,
         null=True,
-        related_name="second_reporting_manager",
+        related_name="second_reporting_employees",
     )
 
     resign_date = models.DateField(null=True)
@@ -222,4 +255,3 @@ class ResetPasswordToken(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     token = models.CharField(max_length=10)
     created_at = models.DateTimeField(auto_now_add=True)
-
--- src/server/backend/users/views.py
+++ src/server/backend/users/views.py
@@ -1,3 +1,118 @@
+import secrets
+
+from _datetime import timedelta
+from django.db.models.query_utils import Q
 from django.shortcuts import render
+from django.utils import timezone
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.generics import get_object_or_404
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from backend.users.models import AccessToken, Company, Profile, RefreshToken, User
+
+from .backend import decode_jwt_token, encode_jwt_token
 
 # Create your views here.
+
+
+class TokenView(APIView):
+    def _invalid_grant_response(self):
+        return Response(
+            data={
+                "msg": "Ivalid Grant",
+            },
+            status=status.HTTP_400_BAD_REQUEST,
+        )
+
+    def _generate_token_response(self, user, grant_type):
+        access_token = AccessToken.objects.create(
+            user=user,
+            token=secrets.token_urlsafe(),
+            expires=timezone.now() + timedelta(seconds=172800),
+        )
+        refresh_token = RefreshToken.objects.create(
+            user=user,
+            access_token=access_token,
+            token=secrets.token_urlsafe(),
+        )
+        return Response(
+            data={
+                "access_token": encode_jwt_token(
+                    expires_in=172800,
+                    token_id=access_token.id,
+                    token_provider="password",
+                    grant_type="access_token",
+                    user_id=user.id,
+                ),
+                "refresh_token": encode_jwt_token(
+                    expires_in=31449600,
+                    token_id=refresh_token.id,
+                    token_provider="password",
+                    grant_type="refresh_token",
+                    user_id=user.id,
+                ),
+                "expire": 172800,
+                "grant_type": grant_type,
+            },
+            status=status.HTTP_200_OK,
+        )
+
+    def post(self, request):
+        grant_type = request.data.get("grant_type", "access_token")
+        if grant_type == "access_token":
+            username = request.data.get("username")
+            email = request.data.get("email")
+            password = request.data.get("password")
+            typeOfEmployee = request.data.get("typeOfEmployee", "INDIRECT")
+
+            try:
+                user = User.objects.get(
+                    Q(username=username) & Q(email=email) & Q(is_active=True)
+                )
+                profile = get_object_or_404(
+                    Profile, user=user, typeOfEmployee=typeOfEmployee
+                )
+                if user.check_password(password):
+                    return self._generate_token_response(user, grant_type)
+                return self._invalid_grant_response()
+
+            except User.DoesNotExist:
+                return self._invalid_grant_response()
+
+        elif grant_type == "refresh_token":
+            refresh_token = decode_jwt_token(request.data.get("refresh_token"))
+            token = RefreshToken.objects.get(id=refresh_token["token_id"])
+            if token.revoked is None:
+                token.revoke()
+                return self._generate_token_response(token.user, grant_type)
+
+            else:
+                return self._invalid_grant_response()
+
+        else:
+            return self._invalid_grant_response()
+
+
+@api_view(["POST"])
+def create_admin_user(request):
+    username = request.data.get("username", "superadmin")
+    email = request.data.get("email", "admin@pms.com")
+    contact_number = request.data.get("contact_number", "")
+    password = request.data.get("password", "password")
+
+    domain = request.data.get("domain", "localhost:3000")
+    company_name = request.data.get("company_name", "admin")
+    user = User.objects.create_superadmin(
+        username=username, email=email, contact_number=contact_number, password=password
+    )
+    user.company = Company.objects.create(name=company_name, domain=domain)
+    user.save()
+
+    profile = Profile.objects.create(user=user)
+    profile.first_reporting_manager = profile
+    profile.second_reporting_manager = profile
+    profile.save()
+
+    return Response(status=201, data={"msg": "Superadmin is successfully created"})
