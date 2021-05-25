0966a0fc0064efc1458456b4eeb1f50e37b28c1f
Tejpratap <tejpratapsingh545@outlook.com>
2021-05-25 16:59:46 +0530

    chack email , password,username

--- src/server/backend/users/api/views.py
+++ src/server/backend/users/api/views.py
@@ -1,7 +1,8 @@
 from backend.appraisals.models import OverAllAppraisal
 from django.db.models import Count, OuterRef, Subquery
-from rest_framework import generics, viewsets
+from rest_framework import generics, status, viewsets
 from rest_framework.permissions import IsAdminUser, IsAuthenticated
+from rest_framework.response import Response
 
 from ..models import *
 from ..permissions import *
@@ -72,3 +73,48 @@ class PermissionViewSet(viewsets.ModelViewSet):
             queryset = queryset.filter(company=self.request.user.company)
 
         return queryset
+
+
+@api_view(["POST"])
+@permission_classes([IsAuthenticated])
+def check_username(request):
+    try:
+        username = request.data.get("username")
+        if not User.objects.filter(username=username).exists():
+            return Response("username is available")
+        return Response("username not is available", status=status.HTTP_400_BAD_REQUEST)
+    except:
+        return Response("username not is available", status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(["POST"])
+@permission_classes([IsAuthenticated])
+def check_password(request):
+    password = request.data.get("password")
+    if check_password_strength(password):
+        return Response("Password is good")
+    return Response("Weak Password", status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(["POST"])
+@permission_classes([IsAuthenticated])
+def check_email(request):
+    try:
+        email = request.data.get("email")
+        if not User.objects.filter(email=email).exists():
+            return Response("email is available")
+        return Response("email not is available", status=status.HTTP_400_BAD_REQUEST)
+    except:
+        return Response("email not is available", status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(["POST"])
+@permission_classes([IsAuthenticated])
+def check_contact_number(request):
+    try:
+        contact_number = request.data.get("contact_number")
+        if not User.objects.filter(contact_number=contact_number).exists():
+            return Response("email is available")
+        return Response("email not is available", status=status.HTTP_400_BAD_REQUEST)
+    except:
+        return Response("email not is available", status=status.HTTP_400_BAD_REQUEST)
--- src/server/config/api_urls.py
+++ src/server/config/api_urls.py
@@ -1,4 +1,9 @@
 from backend.users.api.views import *
 from django.urls.conf import path
 
-urlpatterns = [path("profile/me", ProfileInfoView.as_view())]
+urlpatterns = [
+    path("profile/me", ProfileInfoView.as_view()),
+    path("check/username", check_username),
+    path("check/email", check_email),
+    path("check/password", check_password),
+]
